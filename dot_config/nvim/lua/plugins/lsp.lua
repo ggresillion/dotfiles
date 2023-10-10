4de0e9973b071d78f1a56e30b9117bb9e962bd90
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2023-10-10 22:39:46 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,39 @@
 return {
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'},
+        },
+    },
+
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        cmd = 'LspInfo',
+        event = {'BufReadPre', 'BufNewFile'},
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+            {
+                'williamboman/mason-lspconfig.nvim',
+                config = function()
+                    require('mason-lspconfig').setup({
+                        handlers = {
+                            require('lsp-zero').default_setup,
+                        }
+                    })
+                end
+            },
+            {
+                'williamboman/mason.nvim',
+                config = function()
+                    require('mason').setup({})
+                end
+            },
+        },
+    },
 }
