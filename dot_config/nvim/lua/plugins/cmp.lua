f1d56d2d0f404bf3cab4792d96a9117e190848fe
Guillaume Grésillion <guillaume.gresillion@thalesgroup.com>
2023-10-30 09:36:56 +0100

    split cmp

---
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,36 @@
+return {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+        'L3MON4D3/LuaSnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+    },
+    config = function()
+        local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
+        cmp.setup({
+            mapping = {
+                ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+                ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                ['<Tab>'] = cmp_action.luasnip_supertab(),
+                ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+            },
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'nvim_lsp_signature_help' },
+            },
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+        })
+    end,
+}
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -9,8 +9,6 @@ return {
             vim.g.lsp_zero_extend_lspconfig = 0
         end
     },
-
-    -- LSP
     {
         'neovim/nvim-lspconfig',
         cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
@@ -36,6 +34,15 @@ return {
                 handlers = {
                     lsp_zero.default_setup,
                     gopls = lsp_zero.noop,
+                    jdtls = function()
+                        require('lspconfig').jdtls.setup({
+                            cmd = {
+                                "jdtls",
+                                "--jvm-arg=" ..
+                                string.format("-javaagent:%s", vim.fn.expand "$MASON/share/jdtls/lombok.jar"),
+                            },
+                        })
+                    end
                 }
             })
         end
@@ -43,32 +50,5 @@ return {
     {
         'williamboman/mason.nvim',
         config = true
-    },
-    -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            { 'L3MON4D3/LuaSnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-        },
-        config = function()
-            local cmp = require('cmp')
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_cmp()
-            local cmp_action = require('lsp-zero').cmp_action()
-            cmp.setup({
-                mapping = cmp.mapping.preset.insert({
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<Tab>'] = cmp_action.tab_complete(),
-                    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-                })
-            })
-        end,
     },
 }
