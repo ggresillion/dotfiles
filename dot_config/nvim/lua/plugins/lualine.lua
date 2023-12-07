2e86a5fa5dfe45b0a4422b2ad689e1db864bda7f
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2023-12-07 09:47:30 +0100

    Add .config/nvim/lua/plugins/indent.lua Update .config/nvim/lua/plugins/lualine.lua Update .config/nvim/lua/plugins/theme.lua Add .config/nvim/lua/plugins/ui.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,33 @@
 return {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     opts = {
         options = {
-            theme = 'catppuccin',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-        }
-    }
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
+            globalstatus = true,
+        },
+        sections = {
+            lualine_a = {
+                {
+                    "mode",
+                    fmt = string.lower,
+                },
+            },
+            lualine_c = {
+                {
+                    "filename",
+                    path = 1,
+                },
+            },
+            lualine_z = {
+                {
+                    "location",
+                    icon = "",
+                },
+            },
+        },
+        extensions = {
+            "nvim-tree"
+        },
+    },
 }
