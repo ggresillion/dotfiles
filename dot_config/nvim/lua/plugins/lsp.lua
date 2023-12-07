51ab51338850d0568dee309a57c3562ec817b839
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2023-12-07 10:54:25 +0100

    Update .config/nvim/lua/plugins/lsp.lua Update .config/nvim/lua/plugins/ui.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -19,9 +19,11 @@ return {
             },
         },
         config = function()
+            -- workaround to prevent lsp-zero to set handlers and conflict with noice
+            vim.g.lsp_zero_ui_float_border = 0
             local lsp_zero = require("lsp-zero")
             lsp_zero.extend_lspconfig()
-            lsp_zero.on_attach(function(client, bufnr)
+            lsp_zero.on_attach(function(_, bufnr)
                 lsp_zero.default_keymaps({
                     buffer = bufnr,
                     preserve_mappings = false,
