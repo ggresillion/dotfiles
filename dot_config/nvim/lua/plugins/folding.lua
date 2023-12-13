a17c39a97487b487d7f9ec5249b4a92f53a61def
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2023-12-13 21:37:13 +0100

    Update .config/nvim/lua/plugins/folding.lua

--- dot_config/nvim/lua/plugins/folding.lua
+++ dot_config/nvim/lua/plugins/folding.lua
@@ -28,9 +28,30 @@ end
 
 return {
     "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
+    dependencies = {
+        "kevinhwang91/promise-async",
+        {
+            "luukvbaal/statuscol.nvim",
+            config = function()
+                local builtin = require("statuscol.builtin")
+                require("statuscol").setup(
+                    {
+                        relculright = true,
+                        segments = {
+                            { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
+                            { text = { "%s" },                  click = "v:lua.ScSa" },
+                            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" }
+                        }
+                    }
+                )
+            end
+        }
+    },
     event = "VeryLazy",
     opts = {
+        provider_selector = function()
+            return { "treesitter", "indent" }
+        end,
         close_fold_kinds = { "imports" },
         fold_virt_text_handler = handler,
         preview = {
@@ -64,26 +85,10 @@ return {
     },
     init = function()
         local vim = vim
-        vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+        vim.o.foldenable = true
         vim.o.foldcolumn = "1" -- '0' is not bad
         vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
+        vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
     end,
-    config = function(_, opts)
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.foldingRange = {
-            dynamicRegistration = false,
-            lineFoldingOnly = true
-        }
-        local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-        for _, ls in ipairs(language_servers) do
-            require('lspconfig')[ls].setup({
-                capabilities = capabilities
-                -- you can add other fields for setting up lsp server in this table
-            })
-        end
-        require("ufo").setup(opts)
-    end
 }
-
