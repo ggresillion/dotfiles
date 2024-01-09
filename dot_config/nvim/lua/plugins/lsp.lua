a6cb7a4872baaefea5386ccd91fdb4c058d0f43f
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2024-01-09 21:46:31 +0100

    Update .config/nvim/lua/plugins/lsp.lua Update .config/nvim/lua/plugins/ui.lua Update .config/nvim/lua/remap.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -5,9 +5,12 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             {
-
-                "VonHeikemen/lsp-zero.nvim",
-                "williamboman/mason-lspconfig.nvim",
+                {
+                    "williamboman/mason-lspconfig.nvim",
+                    opts = {
+                        ensure_installed = {},
+                    },
+                },
                 {
                     "williamboman/mason.nvim",
                     opts = {},
@@ -21,7 +24,7 @@ return {
             { "gr",         function() require("telescope.builtin").lsp_references() end,                           desc = "References" },
             { "gi",         function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,  desc = "Goto Implementation" },
             { "go",         function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
-            { "K",          function() vim.lsp.buf.hover() end,                                                     desc = "Hover" },
+            { "K",          vim.lsp.buf.hover,                                                                      desc = "Hover" },
             { "gs",         vim.lsp.buf.signature_help,                                                             desc = "Signature Help", },
             { "<c-k>",      vim.lsp.buf.signature_help,                                                             desc = "Signature Help",        mode = "i", },
             { "<leader>ca", vim.lsp.buf.code_action,                                                                desc = "Code Action",           mode = { "n", "v" } },
@@ -29,57 +32,66 @@ return {
             { "<leader>cr", vim.lsp.buf.rename,                                                                     desc = "Rename", },
             { "<leader>cd", vim.diagnostic.open_float,                                                              desc = "Line Diagnostic" },
         },
-        init = function()
-            vim.g.lsp_zero_ui_float_border = 0
-        end,
         config = function()
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_lspconfig()
-            require("mason-lspconfig").setup({
-                handlers = {
-                    lsp_zero.default_setup,
-                }
-            })
-        end
+            require("mason-lspconfig").setup_handlers {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {}
+                end,
+            }
+        end,
     },
     {
         "hrsh7th/nvim-cmp",
+        version = false,
         event = "InsertEnter",
         dependencies = {
-            "L3MON4D3/LuaSnip",
             "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
         },
+        init = function()
+            vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+        end,
         opts = function()
             local cmp = require("cmp")
-            local cmp_action = require("lsp-zero").cmp_action()
             return {
                 mapping = {
-                    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
-                    ["<Down>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+                    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
                     ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-                    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+                    ["<S-CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    }),
                     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                    ["<Tab>"] = cmp_action.luasnip_supertab(),
-                    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
                 },
-                sources = {
+                sources = cmp.config.sources({
                     { name = "nvim_lsp" },
-                    { name = "nvim_lsp_signature_help" },
                     { name = "luasnip" },
+                    { name = "path" },
+                }, {
+                    { name = "buffer" },
+                }),
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered()
+                },
+                experimental = {
+                    ghost_text = {
+                        hl_group = "CmpGhostText",
+                    },
+                },
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
                 },
                 snippet = {
                     expand = function(args)
                         require("luasnip").lsp_expand(args.body)
                     end,
                 },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered()
-                }
             }
         end
     },
--- dot_config/nvim/lua/remap.lua
+++ dot_config/nvim/lua/remap.lua
@@ -1,2 +1,5 @@
 -- Global
 vim.g.mapleader = " "
+
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+vim.keymap.set('t', 'kj', [[<C-\><C-n>]])
