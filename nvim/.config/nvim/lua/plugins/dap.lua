7c8d50622daee48becf05618ef938ee9f5c5df11
Guillaume Grésillion <guillaume.gresillion@thetreep.com>
2024-08-08 12:09:11 +0200

    dap setup by language

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -1,36 +1,55 @@
 return {
     {
-        "mfussenegger/nvim-dap",
+        "jay-babu/mason-nvim-dap.nvim",
+        opts = {
+            automatic_installation = true,
+        },
         dependencies = {
-            -- fancy UI for the debugger
             {
-                "rcarriga/nvim-dap-ui",
-                dependencies = {
-                    "nvim-neotest/nvim-nio",
-                },
-                -- stylua: ignore
-                keys = {
-                    { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
-                    { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
-                },
+                "williamboman/mason.nvim",
                 opts = {},
+            },
+            {
+                "mfussenegger/nvim-dap",
                 config = function(_, opts)
-                    -- setup dap config by VsCode launch.json file
-                    -- require("dap.ext.vscode").load_launchjs()
                     local dap = require("dap")
-                    local dapui = require("dapui")
-                    dapui.setup(opts)
-                    dap.listeners.after.event_initialized["dapui_config"] = function()
-                        dapui.open({})
+                    for k, v in pairs(opts.adapters) do
+                        dap.adapters[k] = v
                     end
-                    dap.listeners.before.event_terminated["dapui_config"] = function()
-                        dapui.close({})
-                    end
-                    dap.listeners.before.event_exited["dapui_config"] = function()
-                        dapui.close({})
+                    for k, v in pairs(opts.configurations) do
+                        dap.configurations[k] = v
                     end
                 end,
             },
+            -- fancy UI for the debugger
+            -- {
+            --     "rcarriga/nvim-dap-ui",
+            --     dependencies = {
+            --         "nvim-neotest/nvim-nio",
+            --     },
+            --     -- stylua: ignore
+            --     keys = {
+            --         { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+            --         { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
+            --     },
+            --     opts = {},
+            --     config = function(_, opts)
+            --         -- setup dap config by VsCode launch.json file
+            --         -- require("dap.ext.vscode").load_launchjs()
+            --         local dap = require("dap")
+            --         local dapui = require("dapui")
+            --         dapui.setup(opts)
+            --         dap.listeners.after.event_initialized["dapui_config"] = function()
+            --             dapui.open({})
+            --         end
+            --         dap.listeners.before.event_terminated["dapui_config"] = function()
+            --             dapui.close({})
+            --         end
+            --         dap.listeners.before.event_exited["dapui_config"] = function()
+            --             dapui.close({})
+            --         end
+            --     end,
+            -- },
 
             -- virtual text for the debugger
             {
@@ -48,30 +67,7 @@ return {
                     },
                 },
             },
-
-            -- mason.nvim integration
-            {
-                "jay-babu/mason-nvim-dap.nvim",
-                dependencies = "mason.nvim",
-                cmd = { "DapInstall", "DapUninstall" },
-                opts = {
-                    -- Makes a best effort to setup the various debuggers with
-                    -- reasonable debug configurations
-                    automatic_installation = true,
-
-                    -- You can provide additional configuration to the handlers,
-                    -- see mason-nvim-dap README for more information
-                    handlers = {},
-
-                    -- You'll need to check that you have the required things installed
-                    -- online, please don't ask me how to install them :)
-                    ensure_installed = {
-                        -- Update this to ensure that you have the debuggers for the langs you want
-                    },
-                },
-            },
         },
-        lazy = true,
         keys = {
             {
                 "<leader>dB",
--- nvim/.config/nvim/lua/plugins/languages/go.lua
+++ nvim/.config/nvim/lua/plugins/languages/go.lua
@@ -13,40 +13,83 @@ end
 
 return {
     {
-        "leoluz/nvim-dap-go",
-        lazy = false,
+        "neovim/nvim-lspconfig",
         opts = {
-            delve = {
-                port = 40000,
+            gopls = {},
+            golangci_lint_ls = {
+                handlers = {
+                    ["textDocument/publishDiagnostics"] = convert_errors_to_warnings
+                }
             },
-            dap_configurations = {
-                {
-                    type = "go",
-                    name = "Attach remote",
-                    mode = "remote",
-                    request = "attach",
-                    substitutePath = {
-                        {
-                            from = "${workspaceFolder}",
-                            to = "/go/src/github.com/thetreep/thetreep-api",
-                        },
-                    },
-                },
-            },
-        },
-        keys = {
-            { "<leader>dg", function() require("dap-go").debug_test() end, desc = "Debug test (Go)" },
-        },
-        {
-            "neovim/nvim-lspconfig",
-            opts = {
-                gopls = {},
-                golangci_lint_ls = {
-                    handlers = {
-                        ["textDocument/publishDiagnostics"] = convert_errors_to_warnings
-                    }
-                },
-            }
         }
     },
+    -- {
+    --     "jay-babu/mason-nvim-dap.nvim",
+    --     opts = {
+    --         ensure_installed = { "delve" },
+    --     },
+    -- },
+    {
+        "mfussenegger/nvim-dap",
+        opts = {
+            adapters = {
+                delve = {
+                    type = "server",
+                    host = "127.0.0.1",
+                    port = 38697,
+                },
+            },
+            configurations = {
+                go = {
+                    {
+                        type = "delve",
+                        name = "Debug",
+                        request = "launch",
+                        program = "${file}"
+                    },
+                    {
+                        type = "delve",
+                        name = "Debug test", -- configuration for debugging test files
+                        request = "launch",
+                        mode = "test",
+                        program = "${file}"
+                    },
+                    -- works with go.mod packages and sub packages
+                    {
+                        type = "delve",
+                        name = "Debug test (go.mod)",
+                        request = "launch",
+                        mode = "test",
+                        program = "./${relativeFileDirname}"
+                    }
+                },
+            },
+        },
+    },
+    -- {
+    --     "leoluz/nvim-dap-go",
+    --     lazy = false,
+    --     opts = {
+    --         delve = {
+    --             port = 40000,
+    --         },
+    --         dap_configurations = {
+    --             {
+    --                 type = "go",
+    --                 name = "Attach remote",
+    --                 mode = "remote",
+    --                 request = "attach",
+    --                 substitutePath = {
+    --                     {
+    --                         from = "${workspaceFolder}",
+    --                         to = "/go/src/github.com/thetreep/thetreep-api",
+    --                     },
+    --                 },
+    --             },
+    --         },
+    --     },
+    --     keys = {
+    --         { "<leader>dg", function() require("dap-go").debug_test() end, desc = "Debug test (Go)" },
+    --     },
+    -- },
 }
