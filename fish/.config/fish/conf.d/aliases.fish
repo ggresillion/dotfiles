badf67693df50f96122295942fe56ebaf5b5baf4
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2025-06-06 19:41:12 +0200

    bump

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -2,5 +2,8 @@ abbr -a dc  'docker-compose'
 abbr -a dcu 'docker-compose up -d'
 abbr -a dcd 'docker-compose down'
 abbr -a d   'docker'
+abbr -a v   'nvim'
+abbr -a vi  'nvim'
+abbr -a vim 'nvim'
 
 alias cd="z"
