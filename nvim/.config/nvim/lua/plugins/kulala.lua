7a20e0c062ef5d7e4e4a2473547e134073063315
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2025-08-25 14:05:43 +0200

    bump

---
+++ nvim/.config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"mistweaverco/kulala.nvim",
+		ft = { "http", "rest" },
+		opts = {
+			global_keymaps = true,
+			global_keymaps_prefix = "<leader>r",
+			kulala_keymaps_prefix = "",
+			ui = {
+				max_response_size = 124000,
+			},
+		},
+	},
+}
