abb57f7975fd71ad11825913bc9ca4a2ea00c268
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2025-09-25 17:42:45 +0200

    bump

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,29 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	opts = {
-		transparent_background = true,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+			flavour = "mocha",
+		},
+		init = function()
+			vim.cmd("colorscheme catppuccin")
+		end,
 	},
-	init = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme catppuccin]])
-	end,
+	{
+		"f-person/auto-dark-mode.nvim",
+		opts = {
+			update_interval = 2000,
+			set_dark_mode = function()
+				vim.opt.background = "dark"
+				vim.cmd("colorscheme catppuccin-mocha")
+			end,
+			set_light_mode = function()
+				vim.opt.background = "light"
+				vim.cmd("colorscheme catppuccin-latte")
+			end,
+			fallback = "dark",
+		},
+	},
 }
