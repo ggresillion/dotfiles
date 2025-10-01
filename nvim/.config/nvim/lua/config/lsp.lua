55c3e66b046a1987c225e70043db88e0fd6b514c
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2025-10-01 10:59:25 +0200

    bump

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,12 +1,1 @@
-vim.lsp.enable({
-	"gopls",
-	"luals",
-	"ts_ls",
-	"tailwindcss",
-	"jsonls",
-	"golangci_lint_ls",
-	"nil_ls",
-	"terraformls",
-	"pylsp",
-})
 vim.diagnostic.config({ virtual_text = true })
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,41 @@
 vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/williamboman/mason.nvim",
+	"https://github.com/williamboman/mason-lspconfig.nvim",
 })
+
+local lsps = {
+	"gopls",
+	"lua_ls",
+	"ts_ls",
+	"tailwindcss",
+	"jsonls",
+	"golangci_lint_ls",
+	"nil_ls",
+	"terraformls",
+	"pylsp",
+}
+
+require("mason").setup()
+
+require("mason-lspconfig").setup({
+	ensure_installed = lsps,
+	automatic_installation = true,
+})
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			telemetry = { enable = false },
+		},
+	},
+})
+
+vim.lsp.enable(lsps)
